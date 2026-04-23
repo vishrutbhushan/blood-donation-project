1eadfe385d8e87d461e166aabf1300a6fc63683f
vishrutbhushan <vbhushan75@gmail.com>
2026-04-23 11:36:22 +0530

    changes

--- backend/src/main/java/com/hemo/backend/repository/SearchRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hemo.backend.repository;
-
-import com.hemo.backend.entity.Search;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SearchRepository extends JpaRepository<Search, Long> {
-}
